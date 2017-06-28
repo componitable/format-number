299ceae97ecc3e96f2d30a1c273dbae264986871
Piotr Rogowski <piotr.rogowski@creativestyle.pl>
2017-06-28 12:34:36 +0200

    Remove module declaration from typings file

--- index.d.ts
+++ index.d.ts
@@ -1,27 +1,25 @@
-declare module "format-number" {
-  interface IFormatNumberOptions {
-    negativeType?: 'right' | 'left' | 'brackets' | 'none';
-    negativeLeftSymbol?: string;
-    negativeRightSymbol?: string;
-    negativeLeftOut?: boolean;
-    negativeRightOut?: boolean;
-    prefix?: string;
-    suffix?: string;
-    integerSeparator?: string;
-    decimalsSeparator?: string;
-    decimal?: string;
-    padLeft?: number;
-    padRight?: number;
-    round?: number;
-    truncate?: number;
-  }
-
-  interface IFormatNumberOverrideOptions {
-    noUnits?: boolean;
-    noSeparator?: boolean;
-  }
-
-  export default function formatter(options?: IFormatNumberOptions): format;
-
-  type format = (number: number, overrideOptions?: IFormatNumberOverrideOptions) => string;
+interface IFormatNumberOptions {
+  negativeType?: 'right' | 'left' | 'brackets' | 'none';
+  negativeLeftSymbol?: string;
+  negativeRightSymbol?: string;
+  negativeLeftOut?: boolean;
+  negativeRightOut?: boolean;
+  prefix?: string;
+  suffix?: string;
+  integerSeparator?: string;
+  decimalsSeparator?: string;
+  decimal?: string;
+  padLeft?: number;
+  padRight?: number;
+  round?: number;
+  truncate?: number;
 }
+
+interface IFormatNumberOverrideOptions {
+  noUnits?: boolean;
+  noSeparator?: boolean;
+}
+
+export default function formatter(options?: IFormatNumberOptions): format;
+
+type format = (number: number, overrideOptions?: IFormatNumberOverrideOptions) => string;
